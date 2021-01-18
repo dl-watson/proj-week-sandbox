4c08f833e8d24473a67fe607ac97089976794cdc
dl-watson <dlarkinwatson@gmail.com>
2021-01-18 10:24:23 -0800

    passing view test

--- __tests__/inventory.test.js
+++ __tests__/inventory.test.js
@@ -14,10 +14,16 @@ describe("test inventory routes", () => {
   });
 
   it("allows a user to view their inventory via GET", async () => {
-    const { rows } = await pool.query(`SELECT * FROM game_items`);
+    const { rows } = await pool.query(`SELECT * FROM game_users`);
 
+    // add a key to a user's inventory
+    await request(app).get(
+      `/inventory/add/${rows[0].game_id}/${rows[0].game_user_id}/key`
+    );
+
+    // get that user's inventory
     const res = await request(app).get(
-      `/view/${rows[0].game_id}/${rows[0].user_id}`
+      `/inventory/view/${rows[0].game_id}/${rows[0].game_user_id}`
     );
 
     expect(res.body).toEqual({ inventory: ["key"] });
