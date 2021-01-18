dbb98230f8b9c28b8c471a1be2f6109315962c56
dl-watson <dlarkinwatson@gmail.com>
2021-01-18 10:24:56 -0800

    passing add test

--- __tests__/inventory.test.js
+++ __tests__/inventory.test.js
@@ -13,6 +13,17 @@ describe("test inventory routes", () => {
     return pool.query.end();
   });
 
+  it("allows a user to add an item to their inventory", async () => {
+    const { rows } = await pool.query(`SELECT * FROM game_users`);
+
+    // add a key to a user's inventory
+    const res = await request(app).get(
+      `/inventory/add/${rows[0].game_id}/${rows[0].game_user_id}/key`
+    );
+
+    expect(res.body).toEqual({ inventory: ["key"] });
+  });
+
   it("allows a user to view their inventory via GET", async () => {
     const { rows } = await pool.query(`SELECT * FROM game_users`);
 
